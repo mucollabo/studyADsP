fdf859fbf7a6116bca5d26d41e94a08609932231
Charles An <mucollabo@gmail.com>
2021-05-23 00:44:11 +0900

    ADsP complete

--- studyADsP/Rstudy_ch5.R
+++ studyADsP/Rstudy_ch5.R
@@ -42,3 +42,47 @@ table(predict(iris.tree), train.data$Species)
 # validation with test data
 test.pre <- predict(iris.tree, newdata=test.data)
 table(test.pre, test.data$Species)
+
+# analysis with randomforest
+idx <- sample(2, nrow(iris), replace = TRUE, prob = c(0.7, 0.3))
+train.data <- iris[idx==2,]
+test.data <- iris[idx==1,]
+r.f <- randomForest(Species~., data=train.data, ntree=100, proximity=TRUE)
+install.packages("randomForest")
+library(randomForest)
+table(predict(r.f), train.data$Species)
+plot(r.f)
+
+varImpPlot(r.f)
+pre.rf <- predict(r.f, newdata = test.data)
+table(pre.rf, test.data$Species)
+plot(margin(r.f, test.data$Species))
+
+# Hierarchical Clustering
+idx <- sample(1:dim(iris)[1], 40)
+iris.s <- iris[idx,]
+iris.s$Species <- NULL
+hc <- hclust(dist(iris.s), method="ave")
+plot(hc, hang=1, labels=iris$Species[idx])
+
+# K-means Clustering
+data("iris")
+newiris <- iris
+newiris$Species <- NULL
+kc <- kmeans(newiris, 3)
+
+# compare with results
+table(iris$Species, kc$cluster)
+
+# clustering graph
+plot(newiris[c("Sepal.Length", "Sepal.Width")], col=kc$cluster)
+
+library(arulesViz)
+data(Groceries)
+install.packages("arules")
+install.packages("https://cran.r-project.org/src/contrib/Archive/arules/arules_1.6-7.tar.gz", repos = NULL, type="source")
+install.packages('arulesViz')
+
+inspect(Groceries[1:3])
+rules <- apriori(Groceries, parameter = list(support = 0.01, confidence = 0.3))
+inspect(sort(rules, by=c("lift"), decreasing = TRUE)[1:20])
